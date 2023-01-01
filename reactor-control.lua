83e7654b4c56981a98c3e2c6520667f58cfebf73
JonathanRamosWeigend <jonathan.ramos.weigend@gmail.com>
2023-01-01 14:40:21 -0300

    Stable.

--- reactor-control.lua
+++ reactor-control.lua
@@ -36,6 +36,7 @@ function printRectorInfomation(ri)
     print("   Fuel Conversion:                 ", ri.fuelConversion)
     print("   Fuel Conversion Rate             ", ri.fuelConversionRate)
     print("   Fuel Conversion Max:             ", ri.maxFuelConversion)
+    print("                                    ")
 end
 
 local signalLowFlow = flux.getSignalLowFlow()
@@ -55,28 +56,34 @@ while true do
 
     if (energyInPercent < energyControlValue) then
         print("Reactor instablity detected!")
-        print("Decreasing Signal Low Flow ", signalLowFlow, " with ", signalLowFlowDecrease)
+        print(" --> Decreasing Signal Low Flow ", signalLowFlow, " with ", signalLowFlowDecrease)
         signalLowFlow = signalLowFlow - signalLowFlowDecrease
     else
         if (temperature < maxTemperatureControlValue) then
-            print ("Increasing Signal Low Flow ", signalLowFlow, " with ", signalLowFlowIncrease)
+            print (" --> Increasing Signal Low Flow ", signalLowFlow, " with ", signalLowFlowIncrease)
             signalLowFlow = signalLowFlow + signalLowFlowIncrease
         else
             print("Temperatur too high. Can not increase Signal Low Flow.")
         end
     end
     flux.setSignalLowFlow(signalLowFlow)
+    print("")
 
     print("--- Controlling Field Strength  ------------------------------------------------------------------------------") 
     fieldStrengthInPercent = (reactorInfo.fieldStrength / reactorInfo.maxFieldStrength) * 100
     print("Field Strength in Percent: ", fieldStrengthInPercent)
 
     if (fieldStrengthInPercent < fieldStrengthControlValue) then
+        print(" --> Increasing Shield Signal Low Flow ", signalLowFlowShield, " with ", signalLowFlowIncrease)
         signalLowFlowShield = signalLowFlowShield + signalLowFlowIncrease
     else 
+        print(" --> Decreasing Shield Signal Low Flow ", signalLowFlowShield, " with ", signalLowFlowDecrease)
         signalLowFlowShield = signalLowFlowShield - signalLowFlowDecrease
     end
     flux2.setSignalLowFlow(signalLowFlowShield)
+
+    print("--- Summary  ------------------------------------------------------------------------------") 
+    print("Efficiency in %: ", (signalLowFlow / signalLowFlowShield) * 100
 
     -- Wait until timeout or wait on any key and exit
     e = event.pull(loopFrequencyInSeconds)
